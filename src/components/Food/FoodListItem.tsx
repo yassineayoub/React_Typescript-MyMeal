12c813e3e891622f1e07eea0eada9a598cc387f2
Molto <y.ayoubwd@gmail.com>
2022-10-03 10:30:21 +0200

    feat(Total): calc macro WIP

--- src/components/Food/FoodListItem.tsx
+++ src/components/Food/FoodListItem.tsx
@@ -2,16 +2,17 @@ import { AddIcon, MinusIcon } from '@chakra-ui/icons';
 import { Badge, Box, Button, ListItem, Stack } from '@chakra-ui/react';
 import React from 'react';
 import { FoodItem, Meal } from '../../reducers/mealReducer';
+import FoodCard from './FoodCard';
 
 type FoodListItemProps = {
   foodItem: FoodItem;
-  mealIndex: number,
-  meals: Meal[]
+  mealIndex: number;
+  meals: Meal[];
 };
 
 const FoodListItem = ({ foodItem, mealIndex, meals }: FoodListItemProps) => {
   const { id, foodName, protein, carbs, fat, serving, unit } = foodItem;
-  
+
   const isCheckedFood = (mealIndex: number, foodName: string, id: number) => {
     const value = meals[mealIndex].food.findIndex(
       (meal) => meal.foodName === foodName && meal.id === id
@@ -30,54 +31,15 @@ const FoodListItem = ({ foodItem, mealIndex, meals }: FoodListItemProps) => {
       padding={2}
       flex={1}
       cursor="pointer"
-      // boxShadow={
-      //   'rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px'
-      // }
       boxShadow={isCheckedFood(mealIndex, foodName, id) ? 'outline' : 'md'}
       rounded="lg"
       marginBottom={3}
     >
-      <Box flex={1}>
-        <Badge
-          colorScheme={
-            isCheckedFood(mealIndex, foodName, id) ? 'blue' : 'blackAlpha'
-          }
-          variant="subtle"
-          w={'100%'}
-          padding="1.5"
-          position="relative"
-          marginBottom="5px"
-        >
-          <Stack
-            textAlign="center"
-            display="flex"
-            direction="row"
-            justifyContent="center"
-            spacing={10}
-          >
-            <Box display="flex">{foodName}</Box>
-            <Box
-              sx={{ position: 'absolute', right: '3%' }}
-            >{`/${serving}${unit}`}</Box>
-          </Stack>
-        </Badge>
-        <Stack direction="row" justifyContent="space-between">
-          <Badge
-            padding={0.5}
-            alignItems="center"
-            variant="solid"
-            colorScheme="telegram"
-          >
-            Proteines: {protein}
-          </Badge>
-          <Badge variant="solid" colorScheme="orange">
-            Glucides: {carbs}
-          </Badge>
-          <Badge variant="solid" colorScheme="red">
-            Lipide: {fat}
-          </Badge>
-        </Stack>
-      </Box>
+      <FoodCard
+        foodItem={foodItem}
+        mealIndex={mealIndex}
+        isCheckedFood={isCheckedFood}
+      />
 
       <Button
         marginLeft="6px"
--- src/components/Total/Total.tsx
+++ src/components/Total/Total.tsx
@@ -1,17 +1,84 @@
-import { Button, Text } from '@chakra-ui/react'
-import { Link as RouterLink } from 'react-router-dom'
-import React from 'react'
-import { CheckIcon } from '@chakra-ui/icons'
-import { useAppSelector } from '../../store/hooks'
+import { Box, Button, Divider, Input, Stack, Text } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { CheckIcon } from '@chakra-ui/icons';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import FoodCard from '../Food/FoodCard';
+import FoodMealName from '../Food/FoodMealName';
+import { FoodItem, setUpdatedFoodItem } from '../../reducers/mealReducer';
 
 const Total = () => {
-  const { meals } = useAppSelector((state) => state.meal)
-  console.log(meals)
-  return (
-    <>
-    {meals && meals.map(meal => <Text key={meal.index}>{meal.name}</Text>)}
-    </>
-  )
-}
+  const { meals, myMeal } = useAppSelector((state) => state.meal);
+  const dispatch = useAppDispatch();
+  const [myMeals, setUpdatedMeals] = useState(meals);
+  const [amount, setAmount] = useState<number>(0);
 
-export default Total
+  useEffect(() => {
+    setUpdatedMeals(myMeal);
+  }, [myMeal]);
+
+  const handleSetAmount = (
+    amount: number,
+    mealIndex: number,
+    foodItem: FoodItem
+  ) => {
+    if (amount > 0) {
+      const updatedItem = { ...foodItem };
+      console.log(foodItem.serving);
+      // TODO revoir l'input value
+      updatedItem.carbs = amount * (updatedItem.carbs / updatedItem.serving);
+      updatedItem.fat = amount * (updatedItem.fat / updatedItem.serving);
+      updatedItem.protein =
+        amount * (updatedItem.protein / updatedItem.serving);
+      updatedItem.serving = amount;
+      setAmount(amount);
+      // setUpdate()
+      const updatedMeals = [...myMeals];
+      const foodItemIndex = updatedMeals[mealIndex].food.findIndex(
+        (item) => item.id === foodItem.id
+      );
+      console.log(updatedMeals);
+      // dispatch(setMyMeal(updatedMeals));
+      dispatch(
+        setUpdatedFoodItem({
+          mealIndex: mealIndex,
+          updatedFoodItem: updatedItem,
+          foodItemIndex: foodItemIndex,
+        })
+      );
+    }
+  };
+
+  return (
+    <Box
+      w={['95%', '60%', '40%']}
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      margin="auto"
+      textAlign="center"
+      gap={5}
+    >
+      {myMeals.map(({ food, name, index }) => (
+        <>
+          <FoodMealName key={name} name={name} />
+          {food.map((foodItem) => (
+            <Stack key={foodItem.id} direction="row">
+              <FoodCard foodItem={foodItem} mealIndex={index} />
+              <Input
+                type="number"
+                value={foodItem.serving}
+                onChange={(e) =>
+                  handleSetAmount(+e.target.value, index, foodItem)
+                }
+              />
+            </Stack>
+          ))}
+          <Divider />
+        </>
+      ))}
+    </Box>
+  );
+};
+
+export default Total;
